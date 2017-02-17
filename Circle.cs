84e8c68721b2433331aff59a4f4aab823be70b72
alexanderyakusik <alexander.yakusik@gmail.com>
2017-02-18 00:12:39 +0300

    implemented Circle cls inheriting Ellipse cls

---
+++ Circle.cs
@@ -0,0 +1,7 @@
+﻿namespace class_hierarchy
+{
+    public class Circle : Ellipse
+    {
+        public Circle(int x, int y, int radius) : base (x - radius, y + radius, radius * 2, radius * 2) { }
+    }
+}
