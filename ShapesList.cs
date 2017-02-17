398eb515eca2efd929c7c20b0d67755b22bc2359
alexanderyakusik <alexander.yakusik@gmail.com>
2017-02-18 00:26:24 +0300

    implemented ShapesList cls, allowing to store Dot objects

---
+++ ShapesList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace class_hierarchy
+{
+    class ShapesList
+    {
+        private List<Dot> shapes;
+
+        public ShapesList()
+        {
+            shapes = new List<Dot>();
+        }
+
+        public void Add(Dot shape)
+        {
+            shapes.Add(shape);
+        }
+
+        public void DrawShapes(Graphics g, Brush brush)
+        {
+            shapes.ForEach(item => item.Draw(g, brush));
+        }
+    }
+}
