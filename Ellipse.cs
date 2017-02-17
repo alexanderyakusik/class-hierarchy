109bbb68d511ef2917976a4758d020419e0f7e81
alexanderyakusik <alexander.yakusik@gmail.com>
2017-02-18 00:05:42 +0300

    implemented Ellipse cls inheriting Dot cls

---
+++ Ellipse.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace class_hierarchy
+{
+    public class Ellipse : Dot
+    {
+        public int width { get; }
+        public int height { get; }
+
+        public Ellipse(int x, int y, int width, int height) : base(x, y)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override void Draw(Graphics g, Brush brush)
+        {
+            using (var pen = new Pen(brush))
+            {
+                g.DrawEllipse(pen, x, y, width, height);
+            }
+        }
+    }
+}
