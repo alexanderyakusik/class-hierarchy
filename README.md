ec499f5ac388e4fc81a76d0751f753e419789bf5
alexanderyakusik <alexander.yakusik@gmail.com>
2017-02-17 21:41:03 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+###Repo contains simple program that draws geometric figures, which creation was programmed in code
+
+This program was written in order to practice the OOP class hierarchy.
---
+++ frmMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace class_hierarchy
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
