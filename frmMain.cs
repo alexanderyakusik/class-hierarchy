b0d8ee5057c8e4a45faa2d363397a30a2863b7b1
alexanderyakusik <alexander.yakusik@gmail.com>
2017-02-19 13:37:45 +0300

    removed hardcoded shapes drawing

--- frmMain.cs
+++ frmMain.cs
@@ -12,28 +12,12 @@ namespace class_hierarchy
 
         private void frmMain_Load(object sender, System.EventArgs e)
         {
-            var shapesList = new ShapesList();
-            shapesList.Add(new Square(60, 0, 20));
-            shapesList.Add(new Line(40, 30, 100, 30));
-            shapesList.Add(new Dot(40, 40));
-            shapesList.Add(new Dot(100, 40));
-            shapesList.Add(new Rectangle(60, 40, 20, 10));
-            shapesList.Add(new Circle(25, 40, 5));
-            shapesList.Add(new Circle(115, 40, 5));
-            shapesList.Add(new Ellipse(50, 60, 40, 10));
-
             Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
             using (Graphics g = Graphics.FromImage(bmp))
             {
                 g.Clear(Color.White);
             }
             pictureBox.Image = bmp;
-            using (Graphics g = Graphics.FromImage(pictureBox.Image))
-            {
-                var brush = new SolidBrush(Color.Black);
-                shapesList.DrawAllShapes(g, brush);
-            }
-            pictureBox.Invalidate();
         }
     }
 }
